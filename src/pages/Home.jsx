f45716e01a93fd5c0a5a6391464014790069eb32
Pedroporto87 <pedroporto_8@hotmail.com>
2023-03-26 14:47:04 -0300

    rotas finalizadas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+const navigate = useNavigate()
+const handlePost = () => {
+    return navigate("/posts")
+}
+const handleUsers = () => {
+    return navigate("/users")
+}
+
   return (
     <div>
         <h1>Escolha seu caminho!</h1>
-        <button > Posts</button>
-        <button >Usuários</button>
+        <button onClick={handlePost}>Posts</button>
+        <button onClick={handleUsers}>Users</button>
     </div>
   )
 }
